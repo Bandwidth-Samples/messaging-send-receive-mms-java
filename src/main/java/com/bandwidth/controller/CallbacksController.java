d7e9ae839be596401763c3100f2aeb0c030f2946
Brian <bgome004@gmail.com>
2024-02-19 15:18:34 -0500

    SWI-3494

--- src/main/java/com/bandwidth/controller/CallbacksController.java
+++ src/main/java/com/bandwidth/controller/CallbacksController.java
@@ -1,6 +1,7 @@
 package com.bandwidth.controller;
 
-import com.bandwidth.model.MessageCallback;
+import com.bandwidth.sdk.model.*;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
@@ -30,44 +33,38 @@ public class CallbacksController {
     Logger logger = LoggerFactory.getLogger(CallbacksController.class);
 
     @PostMapping("/outbound/messaging/status")
-    public void statusCallback(@RequestBody MessageCallback[] callbacks) throws IOException, MalformedURLException {
+    public void statusCallback(@RequestBody InboundMessageCallback[] callbacks) throws IOException, MalformedURLException {
 
-        for(MessageCallback callback : callbacks) {
+         for (InboundMessageCallback callback : callbacks) {
             logger.info(callback.getType());
             logger.info(callback.getDescription());
-            switch( callback.getType()) {
-                case "message-sending":
-                    logger.info("message-sending type is only for MMS");
-                    break;
-                case "message-delivered":
-                    logger.info("your message has been handed off to the Bandwidth's MMSC network, but has not been confirmed at the downstream carrier");
-                    break;
+            switch (callback.getType()) {
                 case "message-failed":
                     logger.info("For MMS and Group Messages, you will only receive this callback if you have enabled delivery receipts on MMS. ");
                     break;
                 default:
-                    logger.info("Message type does not match endpoint. This endpoint is used for message status callbacks only.");
+                    logger.info(callback.getType() + " <----> " + callback.getDescription());
                     break;
             }
         }
     }
 
     @PostMapping("/inbound/messaging")
-    public void inboundCallback(@RequestBody MessageCallback[] callbacks) throws IOException, MalformedURLException {
+    public void inboundCallback(@RequestBody InboundMessageCallback[] callbacks) throws IOException, MalformedURLException {
 
-        for(MessageCallback callback : callbacks) {
+        for(InboundMessageCallback callback : callbacks) {
             logger.info(callback.getType());
             logger.info(callback.getDescription());
             switch( callback.getType()) {
                 case "message-received":
                     logger.info("Message from: " + callback.getMessage().getFrom());
-                    logger.info("Message to: " + callback.getMessage().getTo().get(0));
+                    logger.info("Message to: " + callback.getMessage().getTo());
                     logger.info("With media");
                     logger.info(callback.getMessage().getText());
 
                     if(callback.getMessage().getMedia().size() > 0) {
                         // Save the media locally
-                        saveMedia(callback.getMessage().getMedia(), System.getProperty("user.dir").concat("\\"));
+                        saveMedia(callback.getMessage().getMedia(), System.getProperty("user.dir").concat("/"));
                     } else  {
                         logger.info("With NO media");
                     }
@@ -79,26 +76,26 @@ public class CallbacksController {
         }
     }
 
-    private void saveMedia(List<String> medias, String rootPath) throws IOException, MalformedURLException {
+    private void saveMedia(List<URI> medias, String rootPath) throws IOException, MalformedURLException {
+
+	for (URI media : medias) {
+
+            String fileName = rootPath.concat(media.toString().substring(media.toString().lastIndexOf("/media/") + 1));
+            File file = new File(fileName);
+            logger.info("With media, saving to: " + file.getAbsolutePath());
 
         //Auth is needed to download the media from the provided media link
         String userPass = username + ":" + password;
         String encoding = Base64.getEncoder().encodeToString(userPass.getBytes());
 
-        for(String media : medias){
-
-            String fileName = rootPath.concat(media.substring(media.lastIndexOf("/") + 1));
-            File file = new File(fileName);
-            logger.info("With media, saving to: " + file.getAbsolutePath());
-
-            URLConnection uc = new URL(media).openConnection();
+	URLConnection uc = new URL(media.toString()).openConnection();
             uc.setRequestProperty("Authorization", "Basic " + encoding);
 
             try(InputStream in = (InputStream)uc.getInputStream()){
                 FileUtils.copyInputStreamToFile(in, file);
             }
-        }
 
+        }
 
     }
 }
